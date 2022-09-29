b15e16af58f5aafb2a44ffa5169c0e6168f028af
SNSerHello <hello.sns@hotmail.com>
2022-09-29 10:14:09 +0800

    [09/29/2022] 使用内联函数更好的代码可读性
    使用内联函数更好的代码可读性

--- matplotlib.h
+++ matplotlib.h
@@ -1,7 +1,8 @@
-#ifndef _MATPLOTLIB_H
-#define _MATPLOTLIB_H
+#ifndef _XXX_YYY_ZZZ_MATPLOTLIB_H
+#define _XXX_YYY_ZZZ_MATPLOTLIB_H
 
 #include <iostream>
+#include <memory>
 #include <sstream>
 #include <string>
 
@@ -9,13 +10,9 @@
 #define __XXX_YYY_ZZZ_POPEN___ _popen
 #define __XXX_YYY_ZZZ_PCLOSE___ _pclose
 #else
-#include <memory>
 #define __XXX_YYY_ZZZ_POPEN___ popen
 #define __XXX_YYY_ZZZ_PCLOSE___ pclose
 #endif
-
-#define PY_MAJOR_VERSION 3
-#include "matplotlibcpp.h"
 
 std::string
 __XXX_YYY_ZZZ_STRIP__(const std::string &str,
@@ -26,7 +23,6 @@ __XXX_YYY_ZZZ_STRIP__(const std::string &str,
     return "";
   }
   size_t to = str.find_last_not_of(whitespace);
-  assert(to != std::string::npos);
 
   return str.substr(from, (to - from) + 1);
 }
@@ -57,26 +53,42 @@ std::string __XXX_YYY_ZZZ_COMMAND__(const std::string &cmd) {
 }
 
 static bool __XXX_YYY_ZZZ_INIT__() {
-  if (getenv("CONDA_PREFIX") == NULL) {
+  auto myenv = [](const std::string &name) {
+    const char *value = getenv(name.c_str());
+    return std::string(value == NULL ? "" : value);
+  };
+  const std::string CONDA_PREFIX = myenv("CONDA_PREFIX");
+  if (CONDA_PREFIX.empty()) {
     // do default python config
   } else {
     const std::string PYTHON_VERSION = __XXX_YYY_ZZZ_COMMAND__(
         "python3 --version | cut -d ' ' -f2 | cut -d '.' -f-2");
+#ifdef _WIN32
+    const std::string PYTHONHOME = CONDA_PREFIX + R"(\lib)";
+    const std::string PYTHONPATH = PYTHONHOME + ";" + PYTHONHOME +
+                                   R"(\site-packages)";
+    _putenv_s("PYTHONHOME", PYTHONHOME.c_str());
+    _putenv_s("PYTHONPATH", PYTHONPATH.c_str());
+#else
     const std::string PYTHONHOME =
-        std::string(getenv("CONDA_PREFIX")) + "/lib/python" + PYTHON_VERSION;
+        CONDA_PREFIX + "/lib/python" + PYTHON_VERSION;
     const std::string PYTHONPATH = PYTHONHOME + ":" + PYTHONHOME +
                                    "/site-packages:" + PYTHONHOME +
                                    "/lib-dynload";
     setenv("PYTHONHOME", PYTHONHOME.c_str(), 1);
     setenv("PYTHONPATH", PYTHONPATH.c_str(), 1);
+#endif
     std::cout << "PYTHON_VERSION: " << PYTHON_VERSION << std::endl;
-    std::cout << "PYTHONHOME    : " << getenv("PYTHONHOME") << std::endl;
-    std::cout << "PYTHONPATH    : " << getenv("PYTHONPATH") << std::endl;
+    std::cout << "PYTHONHOME    : " << myenv("PYTHONHOME") << std::endl;
+    std::cout << "PYTHONPATH    : " << myenv("PYTHONPATH") << std::endl;
   }
 
   return true;
 }
 
 static bool __UNUSED_XXX_YYY_ZZZ_INIT__ = __XXX_YYY_ZZZ_INIT__();
+
+#define PY_MAJOR_VERSION 3
+#include "matplotlibcpp.h"
 
 #endif
